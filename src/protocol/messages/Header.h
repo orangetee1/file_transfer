7217cb82a8f275fc5953958a7daf05b19ba71a44
orangetee1 <zhusalin519@gmail.com>
2025-10-11 15:11:51 +0700

    Header struct creation: created struct for message

---
+++ src/protocol/messages/Header.h
@@ -0,0 +1,15 @@
+//
+// Created by orangetee on 10/11/25.
+//
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <cstdint>
+
+struct Header {
+    uint8_t type;
+    uint32_t length;
+};
+
+#endif //HEADER_H
