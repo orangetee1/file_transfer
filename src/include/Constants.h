ec3f7eb5bd21c0c457cfde41db576a16a91011ac
orangetee1 <zhusalin519@gmail.com>
2025-10-11 16:47:32 +0700

    update: new constant for server folder name

--- src/include/Constants.h
+++ src/include/Constants.h
@@ -6,9 +6,12 @@
 #define CONSTANTS_H
 
 #include <cstdint>
+#include <string>
 
 constexpr uint8_t kDefaultBacklog = 10;
 constexpr uint16_t kMaxFileNameSize = 4096;
 constexpr uint64_t kMaxFileSize = 10e12;
+
+constexpr std::string kFolderName = "uploads";
 
 #endif //CONSTANTS_H
