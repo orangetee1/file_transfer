6d6b813af7c282901a104e455373f4842bf3c2c0
orangetee1 <zhusalin519@gmail.com>
2025-10-11 11:35:46 +0700

    Added a wrapper over the client socket

---
+++ src/net/ClientSocket.cpp
@@ -0,0 +1,27 @@
+//
+// Created by orangetee on 10/11/25.
+//
+
+#include "ClientSocket.h"
+
+#include <stdexcept>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+ClientSocket::ClientSocket() {
+    if ((client_socket_ = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        throw std::runtime_error("socket failed");
+    }
+}
+
+void ClientSocket::setConnection(std::string &address, int port) {
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    inet_pton(AF_INET, address.c_str(), &server_address.sin_addr);
+
+    if (connect(client_socket_, reinterpret_cast<sockaddr*>(&server_address),
+        sizeof(server_address)) < 0) {
+        throw std::runtime_error("connect failed");
+    }
+}
---
+++ src/net/ClientSocket.h
@@ -0,0 +1,21 @@
+//
+// Created by orangetee on 10/11/25.
+//
+
+#ifndef CLIENTSOCKET_H
+#define CLIENTSOCKET_H
+
+#include <string>
+
+class ClientSocket {
+public:
+    ClientSocket();
+    void setConnection(std::string& address, int port);
+
+private:
+    int client_socket_;
+};
+
+
+
+#endif //CLIENTSOCKET_H
