5ed0803ea586307e9b3c20566a955bf78ef860aa
orangetee1 <zhusalin519@gmail.com>
2025-10-11 23:51:08 +0700

    fix: removed use of non-existent classes

--- src/server/ConnectionHandler.cpp
+++ src/server/ConnectionHandler.cpp
@@ -7,8 +7,6 @@
 #include <iostream>
 #include <sys/socket.h>
 
-#include "../protocol/io/MessagesIO.h"
-
 ConnectionHandler::ConnectionHandler(int client_socket, int server_socket,
                                      std::shared_ptr<ServerState> ss) :
     client_socket_(client_socket), server_socket_(server_socket),
@@ -32,24 +30,7 @@ void ConnectionHandler::handle() {
 }
 
 void ConnectionHandler::handle_() {
-    int status;
 
-    Header header = MessagesIO::recvHeader(client_socket_, &status);
-    if (status == 0) {
-        connectionClosedByPeer_();
-    }
-
-    switch (header.type) {
-        case MessageType::INIT:
-            processInitMessage_();
-            break;
-        case MessageType::TRANS:
-            processTransMessage_();
-            break;
-        case MessageType::END:
-            processEndMessage_();
-            break;
-    }
 }
 
 void ConnectionHandler::closeClientSocket_() {
@@ -63,30 +44,4 @@ void ConnectionHandler::closeClientSocket_() {
 void ConnectionHandler::connectionClosedByPeer_() {
     is_transferring_done = true;
     std::cout << "connection closed by peer" << std::endl;
-}
-
-void ConnectionHandler::processInitMessage_() {
-    int status;
-
-    InitMessage init_message = MessagesIO::recvInitMessage(client_socket_, &status);
-    if (status == 0) {
-        connectionClosedByPeer_();
-    }
-
-    file_handler_->openFile(init_message.filename, init_message.filesize);
-}
-
-void ConnectionHandler::processTransMessage_() {
-    int status;
-
-    TransMessage trans_message = MessagesIO::recvTransMessage(client_socket_, &status);
-    if (status == 0) {
-        connectionClosedByPeer_();
-    }
-
-    file_handler_->writeToFile(trans_message.content, kTransferSize);
-}
-
-void ConnectionHandler::processEndMessage_() {
-
 }
