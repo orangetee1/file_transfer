dd59ffdfa37f986a80b66b336263d61c4be96631
orangetee1 <zhusalin519@gmail.com>
2025-10-11 14:10:41 +0700

    Connection handler update: update handler states

--- src/server/ConnectionHandler.cpp
+++ src/server/ConnectionHandler.cpp
@@ -5,18 +5,21 @@
 #include "ConnectionHandler.h"
 
 #include <iostream>
+#include <sys/socket.h>
 
 ConnectionHandler::ConnectionHandler(int client_socket, int server_socket,
-    std::shared_ptr<ServerState> ss) : client_socket_(client_socket),
-server_socket_(server_socket), server_state_(std::move(ss)) {
+                                     std::shared_ptr<ServerState> ss) :
+    client_socket_(client_socket), server_socket_(server_socket),
+    server_state_(std::move(ss)) {
     is_transferring_done = false;
+    is_connection_closed_by_peer_ = false;
 }
 
 // Main work with connection
 void ConnectionHandler::handle() {
-    std::cout << "Thread created" << std::endl;
+    std::cout << "thread created" << std::endl;
 
-    while (!is_transferring_done) {
+    while (!is_transferring_done || !is_connection_closed_by_peer_) {
         handle_();
 
         if (server_state_->getServerState() == 0) {
@@ -29,10 +32,13 @@ void ConnectionHandler::handle() {
 }
 
 void ConnectionHandler::handle_() {
+
 }
 
 void ConnectionHandler::closeClientSocket_() {
     if (close(client_socket_) < 0) {
-        throw std::runtime_error("close failed");
+        std::cout << errno << std::endl;
+
+        throw std::runtime_error("connection handler: close (client socket) failed");
     }
 }
