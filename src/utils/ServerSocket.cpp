a1744199098aeacaedb6ce16471dbf3733c6bbef
orangetee1 <zhusalin519@gmail.com>
2025-10-11 11:22:18 +0700

    Added a wrapper over the server socket

---
+++ src/utils/ServerSocket.cpp
@@ -0,0 +1,30 @@
+//
+// Created by orangetee on 10/11/25.
+//
+
+#include "ServerSocket.h"
+
+#include <stdexcept>
+#include <arpa/inet.h>
+
+#include "../include/Constants.h"
+
+ServerSocket::ServerSocket(int port) {
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if ((server_socket_ = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        throw std::runtime_error("socket failed");
+    }
+
+    if (bind(server_socket_, reinterpret_cast<sockaddr*>(&server_address),
+        sizeof(server_address))) {
+        throw std::runtime_error("bind failed");
+    }
+
+    if (listen(server_socket_, kDefaultBacklog) < 0) {
+        throw std::runtime_error("listen failed");
+    }
+}
---
+++ src/utils/ServerSocket.h
@@ -0,0 +1,20 @@
+//
+// Created by orangetee on 10/11/25.
+//
+
+#ifndef SERVERSOCKET_H
+#define SERVERSOCKET_H
+
+
+
+class ServerSocket {
+public:
+    ServerSocket(int port);
+
+private:
+    int server_socket_;
+};
+
+
+
+#endif //SERVERSOCKET_H
