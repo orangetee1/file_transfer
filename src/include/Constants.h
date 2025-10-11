a2f7afb83bd9259789becbbf138ffb6b27915337
orangetee1 <zhusalin519@gmail.com>
2025-10-11 15:29:18 +0700

    fix: Constants.h overflow

--- src/include/Constants.h
+++ src/include/Constants.h
@@ -5,8 +5,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <cstdint>
+
 constexpr int kDefaultBacklog = 10;
 constexpr int kMaxFileNameSize = 4096;
-constexpr int kMaxFileSize = 10e12;
+constexpr uint64_t kMaxFileSize = 10e12;
 
 #endif //CONSTANTS_H
