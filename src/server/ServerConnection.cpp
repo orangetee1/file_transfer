08572995d3741fc8f429f6818b97cfb002675f32
orangetee1 <zhusalin519@gmail.com>
2025-10-15 14:51:03 +0700

    feat: server connection update

--- src/server/ServerConnection.cpp
+++ src/server/ServerConnection.cpp
@@ -5,23 +5,44 @@
 #include "ServerConnection.h"
 
 #include <iostream>
-#include <sys/socket.h>
+#include <numeric>
 
 #include "../include/Constants.h"
+#include "../protocol/protocol_v1/ProtocolV1Base.h"
+
+std::string typeToString(MessageType& type) {
+    switch (type) {
+        case MessageType::INIT:
+            return "INIT";
+        case MessageType::TRANS:
+            return "TRANS";
+        case MessageType::END:
+            return "END";
+    }
+    return "None";
+}
 
 ServerConnection::ServerConnection(int client_socket, int server_socket,
                                    std::shared_ptr<ServerState> ss) :
     client_socket_(client_socket), server_socket_(server_socket),
     server_state_(std::move(ss)),
     file_handler_(std::make_unique<FileHandler>()),
-    protocol_base_(std::make_unique<ProtocolBase>()) {
+    connection_state_(std::make_unique<ConnectionState>()),
+    client_stats_(std::make_shared<ClientStats>()),
+    protocol_base_(std::make_unique<ProtocolV1Base>(client_socket, connection_state_, client_stats_))
+{
     connection_state_->setConnectionState(1);
+    client_stats_->connected_at = std::chrono::steady_clock::now();
+
+    connection_thread = std::thread(&ServerConnection::handle, this);
 }
 
 void ServerConnection::handle() {
     std::cout << "thread created" << std::endl;
 
-    while (!connection_state_->getConnectionState()) {
+    while (connection_state_->getConnectionState()) {
+        if (!connection_state_->getConnectionState()) break;
+
         handle_();
 
         if (!server_state_->getServerState()) {
@@ -29,51 +50,95 @@ void ServerConnection::handle() {
         }
     }
 
+    auto now = std::chrono::steady_clock::now();
+    if (now - client_stats_->connected_at < std::chrono::seconds(3)) {
+        printSpeedStat_();
+    }
+
     closeClientSocket_();
+    connection_state_->setConnectionState(0);
     std::cout << "thread terminated" << std::endl;
 }
 
 void ServerConnection::handle_() {
     auto header = protocol_base_->getMessageHeader();
+    if (!header) return;
 
     switch (header->type) {
         case MessageType::INIT:
-            processInitMessage_();
+            processInitMessage_(header->packet_length);
             break;
         case MessageType::TRANS:
-            processTransMessage_();
+            processTransMessage_(header->packet_length);
             break;
         case MessageType::END:
-            processEndMessage_();
+            processEndMessage_(header->packet_length);
             break;
     }
 }
 
-void ServerConnection::processInitMessage_() {
-    auto message = protocol_base_->getInitMessage();
+void ServerConnection::processInitMessage_(size_t length) {
+    auto message = protocol_base_->getInitMessage(length);
+
+    file_size_ = message->filesize;
 
     file_handler_->openFile(message->filename, message->filesize);
 
-    // Send init ack
     protocol_base_->sendAckInit();
 }
 
-void ServerConnection::processTransMessage_() {
-    auto message = protocol_base_->getTransMessage();
+void ServerConnection::processTransMessage_(size_t length) {
+    auto message = protocol_base_->getTransMessage(length);
 
     file_handler_->writeToFile(message->content, message->size);
 
-    if (trans_received_count_ % kTransAckStep == 0) {
+    bytes_received_ += message->size;
+
+
+    int c = message->seq / kTransAckStep;
+
+    if (c == 0)
         protocol_base_->sendAckTrans();
+}
+
+ClientStats * ServerConnection::getClientStats() {
+    return client_stats_.get();
+}
+
+void ServerConnection::join() {
+    if (connection_thread.joinable()) {
+        std::cout << "Thread join" << std::endl;
+        connection_thread.join();
     }
 }
 
-void ServerConnection::processEndMessage_() {
-    auto message = protocol_base_->getEndMessage();
+int ServerConnection::getConnectionState() {
+    return connection_state_->getConnectionState();
+}
+
+void ServerConnection::addSpeed(double speed) {
+    speeds_.push_back(speed);
+}
+
+double ServerConnection::getAverageSpeed() {
+    double sum = std::accumulate(speeds_.begin(), speeds_.end(), 0.0);
+    return sum / speeds_.size();
+}
+
+void ServerConnection::processEndMessage_(size_t length) {
+    auto message = protocol_base_->getEndMessage(length);
+
+    bool status = bytes_received_ == file_size_;
+    protocol_base_->sendStatus(status);
 
     connection_state_->setConnectionState(0);
+}
 
-    protocol_base_->sendStatus(status_);
+void ServerConnection::printSpeedStat_() {
+    auto now = std::chrono::steady_clock::now();
+    double connection_time = std::chrono::duration<double>(now - client_stats_->connected_at).count();
+    double average_speed = client_stats_->total_bytes / 1024 / 1024 / connection_time;
+    std::cout << "Average: " << average_speed << " MB/3s\n";
 }
 
 void ServerConnection::closeClientSocket_() {
--- src/server/ServerConnection.h
+++ src/server/ServerConnection.h
@@ -5,18 +5,31 @@
 #define CONNECTIONHANDLER_H
 
 #include <memory>
+#include <thread>
 
-#include "ConnectionState.h"
+#include "ClientStats.h"
+#include "../net/ConnectionState.h"
 #include "FileHandler.h"
 #include "ServerState.h"
 #include "../protocol/ProtocolBase.h"
 
+void alarmSignalHandler();
+
 class ServerConnection {
 public:
     ServerConnection(int client_socket, int server_socket, std::shared_ptr<ServerState> ss);
+    ClientStats* getClientStats();
     void handle();
+    void join();
+    int getConnectionState();
+
+    void addSpeed(double speed);
+    double getAverageSpeed();
 
 private:
+    // Thread
+    std::thread connection_thread;
+
     // Sockets
     int client_socket_;
     int server_socket_;
@@ -24,24 +37,29 @@ private:
     // States
     std::shared_ptr<ServerState> server_state_;
     std::shared_ptr<ConnectionState> connection_state_;
-    bool status_;
+    std::shared_ptr<ClientStats> client_stats_;
 
     // Counts
-    uint32_t trans_received_count_{};
-    uint64_t bytes_received_{};
+    uint32_t trans_received_count_{0};
+    uint64_t bytes_received_{0};
+    uint64_t file_size_{};
 
     // Utils
     std::unique_ptr<FileHandler> file_handler_;
     std::unique_ptr<ProtocolBase> protocol_base_;
+
+    std::vector<double> speeds_;
 
     // Methods
     void handle_();
     void closeClientSocket_();
     void connectionClosedByPeer_();
 
-    void processInitMessage_();
-    void processTransMessage_();
-    void processEndMessage_();
+    void processInitMessage_(size_t length);
+    void processTransMessage_(size_t length);
+    void processEndMessage_(size_t length);
+
+    void printSpeedStat_();
 };
 
 
