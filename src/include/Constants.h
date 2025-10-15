72415ad1bf988449ef09be1b72f2e64e3fc98ac4
orangetee1 <zhusalin519@gmail.com>
2025-10-15 14:48:43 +0700

    update: constants update

--- src/include/Constants.h
+++ src/include/Constants.h
@@ -5,14 +5,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#include <cstdint>
 #include <string>
 
 constexpr uint8_t kDefaultBacklog = 10;
+constexpr uint8_t kHeaderSize = 8;
 constexpr uint16_t kMaxFileNameSize = 4096;
 constexpr uint16_t kTransAckStep = 10;
 constexpr uint64_t kMaxFileSize = 10e12;
-constexpr uint64_t kTransferSize = 256 * 1024;
+constexpr uint64_t kTransferSize = 10 * 1024;
 
 constexpr std::string kFolderName = "uploads";
 
